b75f9ec70f3eb9d5eb8f0a7445ac057ccb241951
vitoschuster <schuster.vito@gmail.com>
2022-06-07 14:55:24 +0200

    First commit

---
+++ src/server.js
@@ -0,0 +1,93 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {  MongoClient } from 'mongodb'; //don't miss the { } import haha
+import path from 'path';
+
+const app = express();
+
+/* prepare for production */
+
+app.use(express.static(path.join(__dirname, '/build')));
+app.use(bodyParser.json());
+
+/**
+ * 
+ * @param {function} operations 
+ * @param {Response} res 
+ */
+const withDB = async (operations, res) => {
+	const client = new MongoClient('mongodb://localhost:27017');
+	try {
+		await client.connect();
+		console.log('connection made');
+		const db = client.db('my-blog'); // connect to my database
+		await operations(db);
+	} catch (error) {
+		res.status(500).json({ message: 'Error conneting to mongoDB', error });
+	} finally {
+		client.close();
+		console.log('connection closed');
+	}
+};
+
+app.get('/api/articles/:name', async (req, res) => {
+  withDB(async (db) => {
+      const articleName = req.params.name;
+      const articleInfo = await db.collection('articles').findOne({ name: articleName })
+      res.status(200).json(articleInfo);
+  }, res);
+})
+
+// function to reuse when working with a database (mongoDB)
+/**
+ * POST - update the post upvote value in the database
+ * @param req request indcoming to the server
+ * @param res response to send to the client - passed inside the funcion
+ * @param db database with an established connection
+ */
+app.post('/api/articles/:name/upvote', async (req, res) => {
+  withDB(async (db) => {
+    const articleName = req.params.name; // get article name
+
+    const articleInfo = await db.collection('articles').findOne({ name: articleName }); //find article
+    await db.collection('articles').updateOne({ name: articleName }, {
+      '$set': {
+        upvotes: articleInfo.upvotes + 1,
+      },
+    });
+
+    const updatedArticle = await db.collection('articles').findOne({ name: articleName });
+
+    res.status(200).json(updatedArticle);
+  }, res);
+	
+});
+
+
+/**
+ * POST - add a comment to the database
+ * @param req request indcoming to the server
+ * @param res response to send to the client - passed inside the funcion
+ * @param db database with an established connection
+ */
+app.post('/api/articles/:name/add-comment', async (req, res) => {
+  const articleName = req.params.name;
+  const { username, text } = req.body;
+  withDB(async (db) => {
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    await db.collection('articles').updateOne({ name: articleName }, {
+      '$set': {
+        comments: articleInfo.comments.concat({ username, text })
+      }
+    })
+    const updatedArticle = await db.collection('articles').findOne({ name: articleName });
+    res.status(200).json(updatedArticle);
+  }, res);
+});
+
+/* ready for production */
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+})
+
+app.listen(8000, () => console.log('Listening on port 8000'));
